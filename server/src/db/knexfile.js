37d41e8d3afc3612a21e54fc5d87f62f4831734c
Forrest Guilloud <fbguillo@gmail.com>
2016-11-30 09:16:33 -0500

    Jaws

--- server/src/db/knexfile.js
+++ server/src/db/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
         host: '127.0.0.1',
         user: 'root',
         password: '',
-        database: process.env.CLEARDB_DATABASE_URL || 'ramen',
+        database: JAWSDB_COBALT_URL || process.env.JAWSDB_COBALT_URL || 'ramen',
     },
     pool: {
         min: 2,
