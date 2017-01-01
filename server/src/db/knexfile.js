4a43a0c9d752d98fb9612de2b1f75f3a0997cddc
Blake Guilloud <fbguillo@gmail.com>
2017-01-01 17:26:24 -0500

    postgres

--- server/src/db/knexfile.js
+++ server/src/db/knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
         host: process.env.PORT || '127.0.0.1',
         user: 'root',
         password: '',
-        database: process.env.CLEARDB_DATABASE_URL || 'ramen',
+        database: process.env.DATABASE_URL || 'ramen',
     },
     pool: {
         min: 2,
