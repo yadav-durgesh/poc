1ca1fa5051f1531791093a38e75e202281030dff
yadav-durgesh <durgeshi.t199@gmail.com>
2020-03-11 12:32:01 +0530

    heroku o Please enter the commit message for your changes. Lines starting

---
+++ heroku-node-react/react-ui/build/precache-manifest.0864720c010ab83e8fd0a863032af8ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88c57f4bb1c5a6c751c3",
+    "url": "/static/css/main.1013375a.chunk.css"
+  },
+  {
+    "revision": "88c57f4bb1c5a6c751c3",
+    "url": "/static/js/main.81e411a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "370b5a7e6a911204316f25b8c52108c6",
+    "url": "/index.html"
+  }
+];
